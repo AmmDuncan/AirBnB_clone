43bc617cb0f01524d6515d769018db1c04e83e1a
ammduncan <ammielgyanyawson@gmail.com>
2022-11-01 07:44:02 +0000

    fix user

--- models/user.py
+++ models/user.py
@@ -7,14 +7,14 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """User model
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
 
     def __init__(self, *args, **kwargs):
         """Initialize User object"""
         super().__init__(*args, **kwargs)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
 
     def decode(self, s):
         """Decode json to instance"""
