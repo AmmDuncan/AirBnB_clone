3b971ebeabdb191132da7cff1e0647858f4be60b
ammduncan <ammielgyanyawson@gmail.com>
2022-11-01 09:10:33 +0000

    fix: test req

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """Test File Storage Engine"""
+import os
 import unittest
+
+from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
     """Test suite for FileStorage engine"""
-    pass
+
+    def test_save(self):
+        """Test save method"""
+        filename = 'file.json'
+        FileStorage._FileStorage__file_path = filename
+        storage_inst = FileStorage()
+        storage_inst.save()
+
+        with open(filename, 'r') as file:
+            self.asset(file.read(), "{}")
+
+        os.remove(filename)
